3742fb5a80e048fc8eb64c8c2162b239a58f7a43
Zyamileva <59625678+Zyamileva@users.noreply.github.com>
2024-02-18 23:32:42 +0000

    Deploying to gh-pages from @ Zyamileva/goit-react-hw-08-phonebook@545c3b1a1a2928c85604b0dbdf391e89c7e3c886 🚀

---
+++ static/js/794.c14a1aa9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,n){n.r(t);var r=n(1087),a=n(3329);t.default=function(){return(0,a.jsxs)(a.Fragment,{children:[" ",(0,a.jsx)("h1",{style:{marginTop:"100px",textAlign:"center"},children:"Cannot found this page"}),(0,a.jsx)(r.rU,{to:"/",style:{marginTop:"100px",display:"flex",justifyContent:"center"},children:"To home page"})]})}}}]);
+//# sourceMappingURL=794.c14a1aa9.chunk.js.map
